1606bde03352ae78407cccfe503751d34bed14eb
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-06-11 01:39:41 -0300

    Suma mensajes para cada error

--- src/main/java/com/example/alumnosApi/services/CursoServices.java
+++ src/main/java/com/example/alumnosApi/services/CursoServices.java
@@ -15,15 +15,14 @@ public class CursoServices {
 
     public List mostrarAlumnos() {
         return ordenarDecrecientementePorApellido(this.curso);
-        //falta ordenar por coso descendiente
     }
 
     public String sumarAlumno(Alumno alumno) {
-        if (validarAlumno(alumno)) {
+        if (validarAlumno(alumno).isBlank()) {
             listaAlumnos.add(alumno);
             return "¡El usuario " + alumno.getNombre() + " " + alumno.getApellido() + " se ha ingresado correctamente!";
         } else {
-            return "La operación falló. Revisar los datos ingresados, ¡Por favor!";
+            return validarAlumno(alumno);
         }
     }
 
@@ -50,37 +49,34 @@ public class CursoServices {
 
     }
 
-    private boolean validarAlumno(Alumno alumno) {
-        boolean usuarioValido = true;
+    private String validarAlumno(Alumno alumno) {
 
-        //chequea que no haya campos vacíos
         if (Objects.isNull(alumno.getApellido()) ||
                 Objects.isNull(alumno.getNombre()) ||
                 Objects.isNull(alumno.isAbonoMatricula()) ||
                 Objects.isNull(alumno.getEdad()) ||
                 Objects.isNull(alumno.getNotaExamenIngreso()) ||
-                Objects.isNull(alumno.isAdeudaMateriasSecundario())) {
-            usuarioValido = false;
-        }
+                Objects.isNull(alumno.isAdeudaMateriasSecundario()))
+            return "ERROR: Ningún campo debe estar vacío.";
 
-        //edad no puede ser numero negativo ni mayor a 100
-        if(alumno.getEdad()<0 || alumno.getEdad()>100) usuarioValido = false;
-        //nombre y apellido no pueden exceder los 20 carácteres
-        if(alumno.getApellido().length() >20 || alumno.getNombre().length() >20) usuarioValido = false;
-        //nota examen debe estar entre 1 y 10
-        if(alumno.getNotaExamenIngreso() <1 || alumno.getNotaExamenIngreso()>10) usuarioValido = false;
+        if (alumno.getEdad() < 0 || alumno.getEdad() > 100)
+            return "ERROR: La edad no puede ser numero negativo ni mayor a 100.";
+        if (alumno.getApellido().length() > 20 || alumno.getNombre().length() > 20)
+            return "ERROR: Ni el nombre ni el apellido debe exceder los 20 carácteres";
+        if (alumno.getNotaExamenIngreso() < 1 || alumno.getNotaExamenIngreso() > 10)
+            return "ERROR: La nota debe ir del 1 al 10";
 
-        return usuarioValido;
+        return "";
     }
 
-    public List<Alumno> ordenarDecrecientementePorApellido(Curso curso){
+    public List<Alumno> ordenarDecrecientementePorApellido(Curso curso) {
         return listaAlumnos.stream().sorted(Comparator.comparing((Alumno a) -> a.getApellido()).reversed()).toList();
     }
 
-    public double obtenerPromedioEdades(){
+    public double obtenerPromedioEdades() {
         double totalEdades = 0;
 
-        for(int i = 0; i<listaAlumnos.size(); i++){
+        for (int i = 0; i < listaAlumnos.size(); i++) {
             totalEdades += listaAlumnos.get(i).getEdad();
         }
 
@@ -88,18 +84,18 @@ public class CursoServices {
     }
 
 
-    public List<Alumno> obtenerAlumnosQueAdeudanMaterias(){
+    public List<Alumno> obtenerAlumnosQueAdeudanMaterias() {
         return listaAlumnos.stream().filter(a -> a.isAdeudaMateriasSecundario() == true).toList();
     }
 
-    public List<Alumno> obtenerAlumnosQueNoAbonaron(){
+    public List<Alumno> obtenerAlumnosQueNoAbonaron() {
         return listaAlumnos.stream().filter(a -> a.isAbonoMatricula() == false).toList();
     }
 
-    public List<Alumno> obtenerAlumnoConMayorNota(){
+    public List<Alumno> obtenerAlumnoConMayorNota() {
         double notaMaxima = 0;
 
-        for (int i = 0; i<this.listaAlumnos.size(); i++){
+        for (int i = 0; i < this.listaAlumnos.size(); i++) {
             double notaIterada = listaAlumnos.get(i).getNotaExamenIngreso();
             if (notaIterada > notaMaxima) {
                 notaMaxima = notaIterada;
