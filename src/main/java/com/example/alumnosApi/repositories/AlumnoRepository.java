22299f88e3feb7d1683515c09099ac348f5b95b0
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-06-13 16:58:10 -0300

    Crea repositorio para alumno

---
+++ src/main/java/com/example/alumnosApi/repositories/AlumnoRepository.java
@@ -0,0 +1,9 @@
+package com.example.alumnosApi.repositories;
+
+import com.example.alumnosApi.entities.Alumno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
+}
