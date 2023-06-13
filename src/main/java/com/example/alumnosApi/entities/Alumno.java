d56c05a70cc6c0b71f9ea7f7a7c630c32e4e0827
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-06-13 16:56:36 -0300

    Suma entidad Alumno

--- src/main/java/com/example/alumnosApi/entities/Alumno.java
+++ src/main/java/com/example/alumnosApi/entities/Alumno.java
@@ -1,15 +1,23 @@
 package com.example.alumnosApi.entities;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class Alumno {
-    private int id;
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String nombre;
     private String apellido;
     private int edad;
--- src/main/java/com/example/alumnosApi/entities/Curso.java
+++ src/main/java/com/example/alumnosApi/entities/Curso.java
@@ -3,6 +3,8 @@ package com.example.alumnosApi.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
