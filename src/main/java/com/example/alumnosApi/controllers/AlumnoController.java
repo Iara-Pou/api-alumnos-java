210bb6ae61f8acd9a492a5b0c3f001f85f924ed9
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-06-15 11:11:20 -0300

    Cambia rutas del get
    Ahora, en lugar de haber 5 rutas diferentes, hay una con parámetros opcionales. Si son verdaderos, devuelve esa información a modo de filtro.

--- src/main/java/com/example/alumnosApi/controllers/AlumnoController.java
+++ src/main/java/com/example/alumnosApi/controllers/AlumnoController.java
@@ -2,50 +2,37 @@ package com.example.alumnosApi.controllers;
 
 import com.example.alumnosApi.entities.Alumno;
 import com.example.alumnosApi.services.AlumnoServices;
+import com.example.alumnosApi.services.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 @RestController
-@RequestMapping
+@RequestMapping("/alumnos")
 public class AlumnoController {
     @Autowired
     private AlumnoServices alumnoServices;
-    @GetMapping("/todos")
-    public List alumnos(@RequestParam boolean adeudaMaterias){
-        if (adeudaMaterias) return alumnoServices.obtenerAlumnosQueAdeudanMaterias();
+    @GetMapping
+    public List obtenerAlumnos(@RequestParam(required = false) boolean ordenApellidoDescendiente,
+                                       @RequestParam(required = false) boolean adeudanMaterias,
+                                       @RequestParam(required = false) boolean abonoPendiente,
+                                       @RequestParam(required = false) boolean promedioEdad,
+                                       @RequestParam(required = false) boolean mayorNota) {
+
+        if (ordenApellidoDescendiente) return alumnoServices.ordenarDecrecientementePorApellido();
+        if (adeudanMaterias) return alumnoServices.obtenerAlumnosQueAdeudanMaterias();
+        if (abonoPendiente) return alumnoServices.obtenerAlumnosQueNoAbonaron();
+        if (promedioEdad) return Collections.singletonList(alumnoServices.obtenerPromedioEdades());
+        if (mayorNota) return alumnoServices.obtenerAlumnoConMayorNota();
+
         return alumnoServices.mostrarAlumnos();
-    }
-//Esta consulta se espera que sea de tipo booleano y se puede pasar en la URL,
-//por ejemplo, /todos?adeudaMaterias=true.
 
-    @GetMapping("/promedioEdad")
-    public double mostrarPromedioEdad(){
-        return alumnoServices.obtenerPromedioEdades();
     }
 
-    @GetMapping("/alumnosQueAdeudanMaterias")
-    public List<Alumno> mostrarAlumnosQueAdeudanMaterias(){
-        return alumnoServices.obtenerAlumnosQueAdeudanMaterias();
-    }
-
-    @GetMapping("/alumnoConMayorNota")
-    public List<Alumno> mostrarAlumnoConMayorNota(){
-        return alumnoServices.obtenerAlumnoConMayorNota();
-    }
-
-    @GetMapping("/alumnosAbonoPendiente")
-    public List<Alumno> mostrarAlumnosAbonoPendiente(){
-        return alumnoServices.obtenerAlumnosQueNoAbonaron();
-    }
-
-    @PostMapping("/sumarAlumno")
-    public String sumarAlumno(@RequestBody Alumno alumno){
-        try{
+    @PostMapping("/crear")
+    public Response sumarAlumno(@RequestBody Alumno alumno){
         return alumnoServices.sumarAlumno(alumno);
-        } catch (Exception e) {
-            return "ERROR: los datos no están ingresados en el formato indicado.";
-        }
     }
 
     @PutMapping("/cambiarAlumno")
