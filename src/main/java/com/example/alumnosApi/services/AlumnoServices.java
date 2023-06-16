ab433d2a543a643c5a7c0327df3a9c93ff0ee467
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-06-15 21:36:13 -0300

    Elimina validaciones
    Ya no son necesarias, al usar exceptions o el ExceptionHandler global

--- src/main/java/com/example/alumnosApi/services/AlumnoServices.java
+++ src/main/java/com/example/alumnosApi/services/AlumnoServices.java
@@ -2,6 +2,7 @@ package com.example.alumnosApi.services;
 
 import com.example.alumnosApi.entities.Alumno;
 import com.example.alumnosApi.entities.Curso;
+import com.example.alumnosApi.exceptions.IdInvalidoException;
 import com.example.alumnosApi.repositories.AlumnoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,76 +23,63 @@ public class AlumnoServices {
         return alumnoRepository.findAll();
     }
 
-    public Response sumarAlumno(Alumno alumno) {
-        Response respuesta = new Response();
-        List error = validarAlumno(alumno);
+    public Alumno sumarAlumno(Alumno alumno) {
+       // Response respuesta = new Response();
+        //List error = validarAlumno(alumno);
 
-        if (error.size() == 0) {
+        //if (error.size() == 0) {
             alumnoRepository.save(alumno);
-            respuesta.setData(Collections.singletonList(alumno));
-        } else {
-            respuesta.setMensajeError(error);
-        }
+       //     respuesta.setData(Collections.singletonList(alumno));
+        //} else {
+        //    respuesta.setMensajeError(error);
+       // }
 
-        return respuesta;
+        return alumno;
     }
 
-    public Response editarAlumno(Long id, Alumno alumno) {
-        Response respuesta = new Response();
-        List<String> mensajeError = new ArrayList<>();
+    public Alumno editarAlumno(Long id, Alumno alumno) {
 
-        if (Objects.isNull(id)) {
-            mensajeError.add("El id no puede estar incompleto.");
-        } else if (!alumnoRepository.existsById(id)) {
-            mensajeError.add("El id no pertenece a ningún campo. Inicializamos el usuario en otro id.");
-        } else {
-            borrarAlumno(id);
+        if (Objects.isNull(id) || !alumnoRepository.existsById(id)) {
+            throw new IdInvalidoException(id);
         }
 
-        Response respuestaCrear = sumarAlumno(alumno);
-        if (!Objects.isNull(respuestaCrear.getMensajeError())) {
-            mensajeError.add(respuestaCrear.getMensajeError().get(0));
-        } else {
-            respuesta.setData(Collections.singletonList(alumno));
-        }
-
-        respuesta.setMensajeError(mensajeError);
-        return respuesta;
+        borrarAlumno(id);
+        sumarAlumno(alumno);
+        //retorna el alumno nuevo
+        return alumno;
     }
 
-    public Response borrarAlumno(Long id) {
-        Response respuesta = new Response();
+    public List<Alumno> borrarAlumno(Long id) {
 
-        if (alumnoRepository.existsById(id)) {
-            respuesta.setData(alumnoRepository.findById(id).stream().toList());
+        if (Objects.isNull(id) || !alumnoRepository.existsById(id)) {
+            throw new IdInvalidoException(id);
+        } else {
             alumnoRepository.deleteById(id);
-        } else {
-            respuesta.setMensajeError(Collections.singletonList("No existe ningún alumno con ese id."));
         }
 
-        return respuesta;
+        //retorna lista de los que quedaron
+        return alumnoRepository.findAll();
     }
 
     private List<String> validarAlumno(Alumno alumno) {
         List errores = new ArrayList<String>();
-        //esto sería mejor como una lista de errores. También están las validaciones de spring though
-        //por cada if sumo un error, retorno lista y el response tiene esa lista como error.
-        if (Objects.isNull(alumno.getApellido()) ||
-                Objects.isNull(alumno.getNombre()) ||
-                Objects.isNull(alumno.isAbonoMatricula()) ||
+
+        //if (Objects.isNull(alumno.getApellido()) ||
+        //        Objects.isNull(alumno.getNombre()) ||
+        /*        Objects.isNull(alumno.isAbonoMatricula()) ||
                 Objects.isNull(alumno.getEdad()) ||
                 Objects.isNull(alumno.getNotaExamenIngreso()) ||
                 Objects.isNull(alumno.isAdeudaMateriasSecundario()))
-            errores.add("Ningún campo debe estar vacío.");
+            errores.add("Ningún campo debe estar vacío.");*/
 
-        if (alumno.getEdad() < 0 || alumno.getEdad() > 100)
-            errores.add("La edad no puede ser numero negativo ni mayor a 100.");
-        if (alumno.getApellido().length() > 20 || alumno.getNombre().length() > 20)
-            errores.add("Ni el nombre ni el apellido debe exceder los 20 carácteres");
-        if (!validarLetrasYEspacios(alumno.getApellido()) || !validarLetrasYEspacios(alumno.getNombre()))
-            errores.add("El nombre y el apellido deben contener solo letras mayúsculas, minúsculas y espacios.");
-        if (alumno.getNotaExamenIngreso() < 1 || alumno.getNotaExamenIngreso() > 10)
-            errores.add("La nota debe ir del 1 al 10");
+        //if (alumno.getEdad() < 0 || alumno.getEdad() > 100)
+        //    errores.add("La edad no puede ser numero negativo ni mayor a 100.");
+        //if (alumno.getApellido().length() > 20 || alumno.getNombre().length() > 20)
+        //    errores.add("Ni el nombre ni el apellido debe exceder los 20 carácteres");
+       // if (!validarLetrasYEspacios(alumno.getApellido()) || !validarLetrasYEspacios(alumno.getNombre()))
+         //   errores.add("El nombre y el apellido deben contener solo letras mayúsculas, minúsculas y espacios.");
+       // if (alumno.getNotaExamenIngreso() < 1 || alumno.getNotaExamenIngreso() > 10)
+        // errores.add("La nota debe ir del 1 al 10");
 
         return errores;
     }
@@ -110,7 +98,6 @@ public class AlumnoServices {
         double totalEdades = alumnoRepository.findAll().stream().mapToInt(Alumno::getEdad).sum();
         return totalEdades / alumnoRepository.findAll().size();
     }
-
 
     public List<Alumno> obtenerAlumnosQueAdeudanMaterias() {
         return alumnoRepository.findAll().stream().filter(a -> a.isAdeudaMateriasSecundario() == true).toList();
