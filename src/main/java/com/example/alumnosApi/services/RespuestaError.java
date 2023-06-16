30a114ebbd72347121ce10c09095fc00ba2ea616
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-06-15 21:36:53 -0300

    Cambia clase Response a RespuestaError
    Ya el response no contiene data y errores. Devuelve una o la otra

---
+++ src/main/java/com/example/alumnosApi/services/RespuestaError.java
@@ -0,0 +1,11 @@
+package com.example.alumnosApi.services;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RespuestaError {
+    private int codigo;
+    private String mensaje;
+}
