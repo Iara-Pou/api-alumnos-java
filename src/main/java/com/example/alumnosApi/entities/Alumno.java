d261384b99fb067e219edf4ecc0471d06a2d7015
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-06-15 21:35:29 -0300

    Suma validaciones a nivel entidad

--- src/main/java/com/example/alumnosApi/entities/Alumno.java
+++ src/main/java/com/example/alumnosApi/entities/Alumno.java
@@ -1,11 +1,17 @@
 package com.example.alumnosApi.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @AllArgsConstructor
 @Entity(name = "alumno")
@@ -15,14 +21,30 @@ public class Alumno {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NonNull
+    @Size(max=20, min = 0)
+    @Pattern(regexp = "^[a-zA-Z ]+$")
     private String nombre;
+
+    @NonNull
+    @Size(max=20, min = 0)
+    @Pattern(regexp = "^[a-zA-Z ]+$")
     private String apellido;
+
+    @Min(0)
+    @Max(100)
     private int edad;
 
     private boolean adeudaMateriasSecundario;
+
+    @NonNull
     private boolean abonoMatricula;
+
+    @Min(0)
+    @Max(10)
     private double notaExamenIngreso;
 
     public Alumno() {
     }
+    //antes tenía un bug porque me faltaba el constructor sin argumentos
 }
