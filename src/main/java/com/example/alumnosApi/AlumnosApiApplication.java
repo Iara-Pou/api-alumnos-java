ea77785df3f9eba29edc2458879512f581003b05
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-06-15 21:33:02 -0300

    Arregla bug
    Antes no podía usar al mismo tiempo @RestController y @ControllerAdvice porque se superponían. Entonces, tuve que importar @ControllerAdvice en el main, para que sea reconocido como un Bean.

--- src/main/java/com/example/alumnosApi/AlumnosApiApplication.java
+++ src/main/java/com/example/alumnosApi/AlumnosApiApplication.java
@@ -1,9 +1,13 @@
 package com.example.alumnosApi;
 
+import com.example.alumnosApi.controllers.ExceptionHandlerGlobal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ExceptionHandlerGlobal.class)
+
 public class AlumnosApiApplication {
 
 	public static void main(String[] args) {
